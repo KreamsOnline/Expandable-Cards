8e8cad80a6341ed8f4c2afd4063b358cd6992da9
AkeemBrown <akeemsterling@hotmail.co.uk>
2021-04-28 18:51:38 +0000

    ADD FILES

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 const cards = document.querySelectorAll('.card-container');
 const cardTitle = document.querySelector('.card-title');
 
-cardTitle.style.opacity = '1';
+cardTitle.style.opacity = "1";
+
+cards.forEach((card) => {
+    card.addEventListener('click', (e) => {
+
+        if(!card.classList.contains('active')){
+            cards.forEach((card) => {
+                card.classList.remove("active");
+                card.childNodes[3].style.opacity = '0';
+            })
+        } 
+        
+        card.classList.toggle("active");
+        card.childNodes[3].style.opacity = '1';
+    })
+})
+
